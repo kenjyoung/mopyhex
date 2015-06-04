64089ded7dc74dc79c9240d11bf24e52295f2147
Mike <kjyoung@ualberta.ca>
2015-06-04 12:44:51 -0600

    updated tournament to record games

--- playtest/tournament.py
+++ playtest/tournament.py
@@ -1,5 +1,11 @@
 from gtpinterface import gtpinterface
 from gamestate import gamestate
+
+def print_game(game):
+	for move in game:
+		print(move, end=' ')
+	print()
+
 def tournament(interface1, interface2, game_number=100, movetime=10, size=8):
 	"""
 	Run some number of games between two agents, alternating who has first move
@@ -12,27 +18,43 @@ def tournament(interface1, interface2, game_number=100, movetime=10, size=8):
 
 	win_count = 0
 	for i in range(game_number):
+		game = []
 		interface1.send_command("clear_board")
 		interface2.send_command("clear_board")
 		if i%2 == 0:
 			while(interface1.send_command("winner")[1] == "none"):
-				interface2.send_command("play w "+interface1.send_command("genmove")[1])
-				interface1.send_command("play b "+interface2.send_command("genmove")[1])
+				move = interface1.send_command("genmove white")
+				if move[0]: 
+					interface2.send_command("play w "+move[1])
+					game.append(move[1])
+				move = interface2.send_command("genmove black")
+				if move[0]:
+					interface1.send_command("play b "+move[1])
+					game.append(move[1])
+
+			print_game(game)
 			if(interface1.send_command("winner")[1] == "white"):
-				print("Game complete, winner: agent1(white)")
+				print("Game complete, winner: agent1(white)\n")
 				win_count+=1
 			else:
-				print("Game complete, winner: agent2(black)")
-				print(interface1.send_command("winner")[1])
+				print("Game complete, winner: agent2(black)\n")
 
 		else:
 			while(interface1.send_command("winner")[1] == "none"):
-				interface1.send_command("play w "+interface2.send_command("genmove")[1])
-				interface2.send_command("play b "+interface1.send_command("genmove")[1])
+				move = interface2.send_command("genmove white")
+				if move[0]: 
+					interface1.send_command("play w "+move[1])
+					game.append(move[1])
+				move = interface1.send_command("genmove black")
+				if move[0]: 
+					interface2.send_command("play b "+move[1])
+					game.append(move[1])
+
+			print_game(game)	
 			if(interface1.send_command("winner")[1] == "black"):
-				print("Game complete, winner: agent1(black)")
+				print("Game complete, winner: agent1(black)\n")
 				win_count+=1
 			else:
-				print("Game complete, winner: agent2(white)")
-				print(interface1.send_command("winner")[1])
+				print("Game complete, winner: agent2(white)\n")
+
 	return win_count/game_number
