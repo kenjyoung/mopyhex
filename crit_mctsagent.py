f15e38a422eb5edb696ef35b33da5dac6a518563
Mike <kjyoung@ualberta.ca>
2015-08-11 10:14:06 -0600

    added crit_mcts.py which uses basic criticality in rollouts

---
+++ crit_mctsagent.py
@@ -0,0 +1,38 @@
+from mctsagent import *
+
+class crit_mctsagent(mctsagent):
+	def __init__(self, state=gamestate(8)):
+		super().__init__(state)
+		self.crit_pts = set()
+
+	def find_crit(self,state):
+		return set()
+
+	def roll_out(self, state):
+		"""Simulate a random game except that we play all known critical
+		cells first, return the winning player and record critical cells at the end."""
+		moves = [x for x in self.crit_pts if state.board[x] == gamestate.PLAYERS["none"]]
+		last = None
+		while(moves):
+			move = random.choice(moves)
+			state.play(move)
+			moves.remove(move)
+			if(state.winner() != gamestate.PLAYERS["none"]):
+				last = move
+				break
+
+		if(state.winner() == gamestate.PLAYERS["none"]):
+			while(True):
+				moves = state.moves()
+				move = random.choice(moves)
+				state.play(move)
+				moves.remove(move)
+				if(state.winner() != gamestate.PLAYERS["none"]):
+					last = move
+					break
+
+		if(last):
+			self.crit_pts.add(last)
+
+		return state.winner()
+
--- playtest/playtest.py
+++ playtest/playtest.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append("../")
 from tournament import tournament
 from mctsagent import mctsagent
+from crit_mctsagent import crit_mctsagent
 from gamestate import gamestate
 from gtpinterface import gtpinterface
 def main():
@@ -10,7 +11,7 @@ def main():
 	for the first agent.
 	"""
 	interface1 = gtpinterface(mctsagent())
-	interface2 = gtpinterface(mctsagent())
+	interface2 = gtpinterface(crit_mctsagent())
 	print(str(tournament(interface1, interface2, 4, 2, 3, ['a1'])))
 	
 
